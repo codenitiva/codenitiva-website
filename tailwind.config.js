4317d4a045f74757cc9288cc5ab9261b144b6728
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-07-10 00:26:00 +0700

    Fix tailwind config conflicting extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {
-      colors: {
-        primary: '#F37335',
-        dark: '#3E2723',
-        muted: '#E6E6E6'
-      }    
-    },
     fontFamily: {
       header: ['Quicksand', 'sans-serif'],
       content: ['Inter', 'sans-serif']
@@ -17,8 +10,8 @@ module.exports = {
         primary: '#FF6F00',
         secondary: '#FFEAD1',
         dark: '#3E2723',
-        text: '#979797',
-        muted: '#D2D2D2'
+        muted: '#979797',
+        light: '#D2D2D2'
       }
     }
   },
