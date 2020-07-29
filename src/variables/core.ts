7e1a04d00a5647f1c21aa3c14aa2b380a096ec0d
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-07-29 16:51:11 +0700

    refactor: refactor core team cards
    Also added new core team member

---
+++ src/variables/core.ts
@@ -0,0 +1,25 @@
+export default {
+  members: [
+    {
+      name: 'Andrew Cen',
+      position: 'Full Stack Developer | Machine Learning Enthusiast | Music Enthusiast',
+      picture: 'https://media-exp1.licdn.com/dms/image/C4E03AQE2CPQI3cenxg/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=xVG-StdEOCFVLSdZ10YRKJX3lwv9tG90d47PyZ7K3VE',
+      iconType: ['github', 'linkedin'],
+      iconURL: ['https://github.com/werdna521', 'https://linkedin.com/linktoandrewcen'],
+    },
+    {
+      name: 'Jayaku Briliantio',
+      position: 'Machine Learning Developer',
+      picture: 'https://media-exp1.licdn.com/dms/image/C5603AQEqzJ1-J7hvRA/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=pefyT7FWAOiZyx6_eTPz0Xi7zoT2_yYsglbFs74eLho',
+      iconType: ['github', 'linkedin'],
+      iconURL: ['https://github.com/ukayaj620', 'https://linkedin.com/neartojayakubriliantio'],
+    },
+    {
+      name: 'Tommy Salim',
+      position: 'UI/UX Designer',
+      picture: 'https://media-exp1.licdn.com/dms/image/C5603AQGC2Eyt4r4zjA/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=28DhLwy2cXEVWYUxRNSNRoeqwSYCsOCjsizTyKdfmss',
+      iconType: ['linkedin'],
+      iconURL: ['https://www.linkedin.com/in/tommy-salim-72870a1a3/'],
+    },
+  ]
+};
