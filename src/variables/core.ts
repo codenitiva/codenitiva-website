7175245794be3fed9355ea84a947c6663fc3ca29
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-08-18 23:56:32 +0700

    feat: update core team member's role

--- src/variables/core.ts
+++ src/variables/core.ts
@@ -2,14 +2,14 @@ export default {
   members: [
     {
       name: 'Andrew Cen',
-      position: 'Full Stack Developer | Machine Learning Enthusiast | Music Enthusiast',
+      position: 'Software Engineer',
       picture: 'https://media-exp1.licdn.com/dms/image/C4E03AQE2CPQI3cenxg/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=xVG-StdEOCFVLSdZ10YRKJX3lwv9tG90d47PyZ7K3VE',
       iconType: ['github', 'linkedin'],
       iconURL: ['https://github.com/werdna521', 'https://linkedin.com/linktoandrewcen'],
     },
     {
       name: 'Jayaku Briliantio',
-      position: 'Machine Learning Developer',
+      position: 'Software Engineer',
       picture: 'https://media-exp1.licdn.com/dms/image/C5603AQEqzJ1-J7hvRA/profile-displayphoto-shrink_400_400/0?e=1601510400&v=beta&t=pefyT7FWAOiZyx6_eTPz0Xi7zoT2_yYsglbFs74eLho',
       iconType: ['github', 'linkedin'],
       iconURL: ['https://github.com/ukayaj620', 'https://linkedin.com/neartojayakubriliantio'],
